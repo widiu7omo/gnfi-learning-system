01f1ead15b0ba0a8ff8395e0cd740bbb97d4aea0
Danar Widi <widiu7omo@gmail.com>
2023-03-28 23:41:33 +0800

    Update components

--- resources/views/components/checkbox.blade.php
+++ resources/views/components/checkbox.blade.php
@@ -1,6 +1,8 @@
-@props(['disabled'=>false,'label'=>'',])
+@props(['disabled'=>false,'label'=>'','checked'=>false])
 <label for="{{$attributes->get('id')}}" class="inline-flex items-center">
     <input type="checkbox"
+           {{$checked?'checked':''}}
+           autocomplete="off"
         {{$disabled?'disabled':''}}
         {{$attributes->merge(['class'=>"rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800"])}}>
     <span class="ml-2 text-sm text-gray-600 dark:text-gray-400 font-semibold">{{ __($label) }}</span>
