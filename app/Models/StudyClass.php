491de2acedc60cf2ad7ee3415095b6caf1d9370f
Danar Widi <widiu7omo@gmail.com>
2023-03-28 23:42:25 +0800

    Add total student count

--- app/Models/StudyClass.php
+++ app/Models/StudyClass.php
@@ -6,8 +6,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\HasManyThrough;
-use phpDocumentor\Reflection\Types\This;
 
 class StudyClass extends Model
 {
@@ -21,7 +19,7 @@ class StudyClass extends Model
 
     public function students(): BelongsToMany
     {
-        return $this->belongsToMany(User::class, 'class_registrations')->withPivot('status');
+        return $this->belongsToMany(User::class, 'class_registrations')->withPivot('status', 'id');
     }
 
     public function class_registrations(): HasMany
@@ -37,5 +35,10 @@ class StudyClass extends Model
     public function is_user_registered(User $user)
     {
         return $this->students->contains($user);
+    }
+
+    public function get_total_students()
+    {
+        return $this->students->filter(fn($item) => $item->pivot->status == 1)->count();
     }
 }
