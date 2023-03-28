6ab0c89907fa5c89a76c41b74884c7d88b80aaa3
Danar Widi <widiu7omo@gmail.com>
2023-03-28 23:44:42 +0800

    Add actions to remove and add bulky

--- app/Http/Controllers/ClassRegistrationController.php
+++ app/Http/Controllers/ClassRegistrationController.php
@@ -16,8 +16,64 @@ class ClassRegistrationController extends Controller
      */
     public function index()
     {
-        $classRegistrations = ClassRegistration::latest()->paginate(5);
+        $classRegistrations = ClassRegistration::paginate(5);
         return view('class-registration.index', compact('classRegistrations'));
+    }
+
+//    public function destroy(ClassRegistration $classRegistration)
+//    {
+//        try {
+//            $classRegistration->delete();
+//            return redirect()->route('class-registration.index')->with('success', 'Class registration deleted successfully');
+//        } catch (QueryException $queryException) {
+//            Log::error($queryException->getMessage());
+//            return redirect()->route('class-registration.index')->with('error', 'Class registration failed to delete');
+//        }
+//    }
+
+    /**
+     * @param StudyClass $studyClass
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     * Add new students from staff role
+     */
+    public function addStudents(StudyClass $studyClass, Request $request)
+    {
+        collect($request->students)->each(function ($student) use ($studyClass) {
+            // Check duplication
+            if (!$studyClass->students->contains($student)) {
+                // Status directly become accepted because manage by staff
+                $studyClass->students()->attach($student, ['status' => 1]);
+            }
+        });
+        return response()->json(['success' => true]);
+    }
+
+    /**
+     * @param StudyClass $studyClass
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     * Remove students bulky by staff
+     */
+    public function removeStudents(StudyClass $studyClass, Request $request)
+    {
+        collect($request->students)->each(function ($student) use ($studyClass) {
+            // Check duplication
+            if ($studyClass->students->contains($student)) {
+                $studyClass->students()->detach($student);
+            }
+        });
+        return response()->json(['success' => true]);
+    }
+
+    public function destroy(ClassRegistration $classRegistration, Request $request)
+    {
+        $student = $classRegistration->student;
+        $classRegistration->delete();
+        if ($request->backUrl != null) {
+            return redirect()->to($request->backUrl)->with('success', $student->name . " has been removed from this class");
+        }
+        return redirect()->route('class-registration.index')->with('success', $student->name . " has been removed class enrollments");
     }
 
     /**
