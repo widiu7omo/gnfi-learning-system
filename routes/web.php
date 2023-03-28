8497032897f662b26d6e908ed9a5c40fe261f35e
Danar Widi <widiu7omo@gmail.com>
2023-03-28 13:22:28 +0800

    Update route

--- routes/web.php
+++ routes/web.php
@@ -35,15 +35,20 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/discover-classes', [StudyClassController::class, 'discoverClasses'])->name('discover-class.index');
-    Route::get('profile/{student}/your-classes', [StudyClassController::class, 'studentClasses'])->name('your-classes');
+    Route::get('/profile/{student}/your-classes', [StudyClassController::class, 'studentClasses'])->name('your-classes');
+    Route::get('/classes/discover', [StudyClassController::class, 'discoverClasses'])->name('discover-class.index');
+    Route::get('/classes/{studyClass}/open', [StudyClassController::class, 'openEnrolledClass'])->name('class-study.open-enrolled-class');
+    Route::get('/classes/{studyClass}/enroll', [ClassRegistrationController::class, 'enroll'])->name('class.enroll');
+    Route::get('/classes/{studyClass}/cancel-enroll', [ClassRegistrationController::class, 'cancelEnroll'])->name('class.cancel-enroll');
     //Admin & Staff
     Route::resource('/study-class', StudyClassController::class)->except('show');
     Route::get('/study-class/{studyClass}/toggle', [StudyClassController::class, 'toggle'])->name('study-class.toggle');
     Route::get('/study-class/{studyClass}/manage-students', [StudyClassController::class, 'manageStudents'])->name('study-class.manage-students');
     Route::resource('/students', StudentController::class)->except('show');
     Route::get('/students/{student}/manage-classes', [StudentController::class, 'manageClass'])->name('students.manage-class');
-    Route::resource('/class-registration', ClassRegistrationController::class)->except('show');
+    Route::resource('/class-registration', ClassRegistrationController::class)->except(['show', 'create', 'update']);
+    Route::post('/class-registration/{classRegistration}/accept', [ClassRegistrationController::class, 'accept'])->name('class-registration.accept');
+    Route::post('/class-registration/{classRegistration}/decline', [ClassRegistrationController::class, 'decline'])->name('class-registration.decline');
 });
 
 require __DIR__ . '/auth.php';
