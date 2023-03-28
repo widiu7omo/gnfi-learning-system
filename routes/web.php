d524c9dd3de78e9f9908c433d63462698d8ca503
Danar Widi <widiu7omo@gmail.com>
2023-03-28 10:08:29 +0800

    Add new routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ClassRegistrationController;
+use App\Http\Controllers\DiscoverClassController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\StudyClassController;
@@ -22,16 +23,27 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    if (auth()->user()->hasRole(['super_admin', 'staff'])) {
+        return view('admin-dashboard');
+    } else {
+        return view('dashboard');
+    }
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    //Student
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::resource('/study-class', StudyClassController::class);
-    Route::resource('/students', StudentController::class);
-    Route::resource('/class-registration', ClassRegistrationController::class);
+    Route::get('/discover-classes', [StudyClassController::class, 'discoverClasses'])->name('discover-class.index');
+    Route::get('profile/{student}/your-classes', [StudyClassController::class, 'studentClasses'])->name('your-classes');
+    //Admin & Staff
+    Route::resource('/study-class', StudyClassController::class)->except('show');
+    Route::get('/study-class/{studyClass}/toggle', [StudyClassController::class, 'toggle'])->name('study-class.toggle');
+    Route::get('/study-class/{studyClass}/manage-students', [StudyClassController::class, 'manageStudents'])->name('study-class.manage-students');
+    Route::resource('/students', StudentController::class)->except('show');
+    Route::get('/students/{student}/manage-classes', [StudentController::class, 'manageClass'])->name('students.manage-class');
+    Route::resource('/class-registration', ClassRegistrationController::class)->except('show');
 });
 
 require __DIR__ . '/auth.php';
